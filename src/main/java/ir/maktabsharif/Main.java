22ff7c928fcf25abb5c50b2590a2189944dd7b5b
Nilu <nilu.shahmoradi@gmail.com>
2025-12-28 21:07:33 +0100

    doc(main): practice entityManager and entityTransaction

--- src/main/java/ir/maktabsharif/Main.java
+++ src/main/java/ir/maktabsharif/Main.java
@@ -1,7 +1,9 @@
 package ir.maktabsharif;
 
+import ir.maktabsharif.models.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -16,6 +18,35 @@ public class Main {
 
             try (EntityManager entityManager = emf.createEntityManager()) {
 
+                // har EntityManager be ye EntityTransaction vabaste hast
+                // Tarakonesh haro ham bayad khodemun modiriat konim
+
+                EntityTransaction entityTransaction = entityManager.getTransaction();
+                entityTransaction.begin();
+
+                User user = new User();
+                user.setUsername("admin");
+                user.setPassword("123");
+
+                // baraye insert kardan
+                // yeki az rah haii ke instance Entity ro mibare tu halate manage (method haye dg: find,remove va retrieve)
+                entityManager.persist(user);
+
+                // aya ye chizi dakhele entityManager hast ya na (aya tu halate manage gharar dare ya na)
+                // javab e in mishe true chon inja rafte tu halate manage: entityManager.persist(user);
+                // pas dg SELECT neminevise (query nemizane) va mige UPDATE, chon entityManager daratesh hanuz ru halate manage.
+                User fetchUser = entityManager.find(User.class, 1L);
+                boolean contains = entityManager.contains(fetchUser);
+                System.out.println(fetchUser);
+                System.out.println("contains: " + contains);
+
+                // sabte taghirat dar DB
+                entityTransaction.commit();
+
+                // detach: az halate manage dg darmiad
+                entityManager.detach(fetchUser);
+                boolean contains1 = entityManager.contains(fetchUser);
+                System.out.println("contains: " + contains1);
             }
 
         }
