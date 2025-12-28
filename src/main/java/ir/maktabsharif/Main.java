9e73c3d2acbe82ebcf3dd5bc414350d17c5b875d
Nilu <nilu.shahmoradi@gmail.com>
2025-12-28 21:26:28 +0100

    doc(main): practice select query and multiple instance in managed state

--- src/main/java/ir/maktabsharif/Main.java
+++ src/main/java/ir/maktabsharif/Main.java
@@ -6,6 +6,8 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -13,40 +15,26 @@ public class Main {
 
         try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("default")) {
 
-            // EntityManager: hameye amaliate DB(actions: Insert, Update, ...) ba in anjam mishe
-            // EntityManagerFactory: sazande/karkhune EntityManager
-
             try (EntityManager entityManager = emf.createEntityManager()) {
 
-                // har EntityManager be ye EntityTransaction vabaste hast
-                // Tarakonesh haro ham bayad khodemun modiriat konim
 
-                EntityTransaction entityTransaction = entityManager.getTransaction();
-                entityTransaction.begin();
+//                EntityTransaction entityTransaction = entityManager.getTransaction();
+//                entityTransaction.begin();
 
-                User user = new User();
-                user.setUsername("admin");
-                user.setPassword("123");
+                // peyda kardane hameye user ha
+                // ba query JPQL: native nist --> bar hasbe esme class e java va esme field haye java
+                List<User> resultList = entityManager.createQuery(
+                        "select u from User u where u.username is not null",
+                        User.class
+                ).getResultList();
 
-                // baraye insert kardan
-                // yeki az rah haii ke instance Entity ro mibare tu halate manage (method haye dg: find,remove va retrieve)
-                entityManager.persist(user);
+                // in bayad ba result yeki bashe:
+                // "select u.username from User u where u.username is not null",
+                // String.class
 
-                // aya ye chizi dakhele entityManager hast ya na (aya tu halate manage gharar dare ya na)
-                // javab e in mishe true chon inja rafte tu halate manage: entityManager.persist(user);
-                // pas dg SELECT neminevise (query nemizane) va mige UPDATE, chon entityManager daratesh hanuz ru halate manage.
-                User fetchUser = entityManager.find(User.class, 1L);
-                boolean contains = entityManager.contains(fetchUser);
-                System.out.println(fetchUser);
-                System.out.println("contains: " + contains);
+                resultList.forEach(user -> System.out.println(user.getId() + " contains in em: " + entityManager.contains(user)));
 
-                // sabte taghirat dar DB
-                entityTransaction.commit();
-
-                // detach: az halate manage dg darmiad
-                entityManager.detach(fetchUser);
-                boolean contains1 = entityManager.contains(fetchUser);
-                System.out.println("contains: " + contains1);
+//                entityTransaction.commit();
             }
 
         }
